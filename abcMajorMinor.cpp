ANGSD/angsd

commit 8e3a499ab1210e9448c163bcb3bf165d53bd9ec0

Moved the 3 possible likelihoods from abcGL to majorminor. To simplyfy the vcf later on

abcMajorMinor.cpp
@@ -23,6 +23,7 @@
 */
 
 
+
 #include <cmath> //<- for log,exp
 #include <cassert>
 #include <cfloat>
@@ -30,6 +31,13 @@
 #include "analysisFunction.h"
 #include "abc.h"
 #include "abcMajorMinor.h"
+
+static int isnaninf(double *d,int l){
+  for(int i=0;i<l;i++)
+    if(std::isnan(d[i])||std::isinf(d[i]))
+      return 1;
+  return 0;
+}
 
 void abcMajorMinor::printArg(FILE *argFile){
   fprintf(argFile,"-------------------\n%s:\n",__FILE__);
@@ -94,14 +102,17 @@ void abcMajorMinor::getOptions(argStruct *arguments){
  doSaf = angsd::getArg("-doSaf",doSaf,arguments);
  pest = angsd::getArg("-pest",pest,arguments);
  skipTriallelic = angsd::getArg("-skipTriallelic",skipTriallelic,arguments);
+ doVcf = angsd::getArg("-doVcf",doVcf,arguments);
+ doGlf = angsd::getArg("-doGlf",doGlf,arguments);
 }
 
 abcMajorMinor::abcMajorMinor(const char *outfiles,argStruct *arguments,int inputtype){
   skipTriallelic=0;
-    doMajorMinor = 0;
+  doMajorMinor = 0;
   doSaf = 0;
   pest = NULL;
-  
+  doVcf = 0;
+  doGlf = 0;
   if(arguments->argc==2){
     if(!strcasecmp(arguments->argv[1],"-doMajorMinor")){
       printArg(stdout);
@@ -118,6 +129,7 @@ abcMajorMinor::abcMajorMinor(const char *outfiles,argStruct *arguments,int input
 }
 
 abcMajorMinor::~abcMajorMinor(){
+
 }
 
 void abcMajorMinor::clean(funkyPars *pars){
@@ -125,6 +137,18 @@ void abcMajorMinor::clean(funkyPars *pars){
     delete [] pars->major;
     delete [] pars->minor;
     pars->major=pars->minor=NULL;
+
+    if(doVcf>0||doGlf==2){
+      lh3struct *lh3 =(lh3struct*) pars->extras[index];
+      for(int i=0;i<pars->numSites;i++)
+	if(lh3->hasAlloced[i])
+	  delete [] lh3->lh3[i];
+      delete [] lh3->lh3;
+      delete [] lh3->hasAlloced;
+      
+      delete lh3;
+    }
+
   }
 }
 
@@ -355,4 +379,40 @@ void abcMajorMinor::run(funkyPars *pars){
   if(doMajorMinor==4||doMajorMinor==5)
     modMajorMinor(pars,doMajorMinor);
   */
+  if(doGlf==2||doVcf>0){
+    lh3struct *lh3 = new lh3struct;
+    pars->extras[index] = lh3;
+    lh3->hasAlloced = new char[pars->numSites];
+    memset(lh3->hasAlloced,0,pars->numSites);
+    lh3->lh3 = new double *[pars->numSites];
+
+    for(int s=0;s<pars->numSites;s++) {
+      if(pars->keepSites[s]==0)
+	continue;
+      int major = pars->major[s];
+      int minor = pars->minor[s];
+      assert(major!=4&&minor!=4);
+      
+      lh3->hasAlloced[s]=1;
+      lh3->lh3[s] = new double[3*pars->nInd];
+
+      for(int i=0;i<pars->nInd;i++) {
+	double norm=exp(pars->likes[s][i*10+angsd::majorminor[major][major]])+exp(pars->likes[s][i*10+angsd::majorminor[major][minor]])+exp(pars->likes[s][i*10+angsd::majorminor[minor][minor]]);
+	double val[3];
+	val[0] = exp(pars->likes[s][i*10+angsd::majorminor[major][major]])/norm;
+	val[1] = exp(pars->likes[s][i*10+angsd::majorminor[major][minor]])/norm;
+	val[2] = exp(pars->likes[s][i*10+angsd::majorminor[minor][minor]])/norm;
+	if(isnaninf(val,3)){
+	  pars->keepSites[s]=0;
+	  break;
+	}
+	lh3->lh3[s][i*3+0]=val[0];
+	lh3->lh3[s][i*3+1]=val[1];
+	lh3->lh3[s][i*3+2]=val[2];
+      }
+      
+
+    }
+  }
+
 }

abcFreq.cpp
@@ -1,11 +1,8 @@
-
 /*
-
   thorfinn@binf.ku.dk
   part of angsd.
 
   gnu license, mit, bsd whatever free license
-
 
   BUGS:
   phat doesn't put data into freq->freq. Maybe this should be fixed, in either case, nobody is using the phat estimator

abcGL.cpp
@@ -33,6 +33,8 @@
 #include "abcGL.h"
 #include "abcError.h"
 #include "phys_likes.h"
+#include "abcMajorMinor.h"
+
 extern int refToInt[256];
 
 static float *logfactorial=NULL;
@@ -100,7 +102,7 @@ void abcGL::getOptions(argStruct *arguments){
   doGlf=angsd::getArg("-doGlf",doGlf,arguments);
   errorFname = angsd::getArg("-errors",errorFname,arguments);
   minInd = angsd::getArg("-minInd",minInd,arguments);
-
+  
 
   int doCounts=0;
   int doMajorMinor =0;
@@ -154,7 +156,6 @@ abcGL::abcGL(const char *outfiles,argStruct *arguments,int inputtype){
   postfix = ".glf.gz";
   beaglepostfix = ".beagle.gz";
   
-
   trim =0;
   GL=0;
   doGlf=0;
@@ -353,7 +354,6 @@ void abcGL::run(funkyPars *pars){
     }
   }
 
-
 }
 
 void abcGL::getLikesFullError10Genotypes(int numSites,int nInd,suint **counts,double ****errorProbs,int *keepSites,double **loglikes) {
@@ -393,13 +393,6 @@ void abcGL::getLikesFullError10Genotypes(int numSites,int nInd,suint **counts,do
 
 }
 
-static int isnaninf(double *d,int l){
-  for(int i=0;i<l;i++)
-    if(std::isnan(d[i])||std::isinf(d[i]))
-      return 1;
-  return 0;
-}
-
 void abcGL::printLike(funkyPars *pars) {
   assert(pars->likes!=NULL);
 
@@ -416,7 +409,8 @@ void abcGL::printLike(funkyPars *pars) {
     //beagle format
     bufstr.l = 0; //set tmpbuf beginning to zero
     for(int s=0;s<pars->numSites;s++) {
-      if(pars->keepSites[s]==0)
+      lh3struct *lh3 = (lh3struct*) pars->extras[index+1];
+      if(pars->keepSites[s]==0||lh3->hasAlloced[s]==0)
 	continue;
       
       kputs(header->target_name[pars->refId],&bufstr);
@@ -430,23 +424,13 @@ void abcGL::printLike(funkyPars *pars) {
       int major = pars->major[s];
       int minor = pars->minor[s];
       assert(major!=4&&minor!=4);
-	
+      
       for(int i=0;i<pars->nInd;i++) {
-	
-	double norm=exp(pars->likes[s][i*10+angsd::majorminor[major][major]])+exp(pars->likes[s][i*10+angsd::majorminor[major][minor]])+exp(pars->likes[s][i*10+angsd::majorminor[minor][minor]]);
-	double val[3];
-	val[0] = exp(pars->likes[s][i*10+angsd::majorminor[major][major]])/norm;
-	val[1] = exp(pars->likes[s][i*10+angsd::majorminor[major][minor]])/norm;
-	val[2] = exp(pars->likes[s][i*10+angsd::majorminor[minor][minor]])/norm;
-	if(isnaninf(val,3)){
-	  pars->keepSites[s]=0;
-	  bufstr.l=0;
-	  break;
-	}
-	ksprintf(&bufstr, "\t%f",val[0]);
-	ksprintf(&bufstr, "\t%f",val[1]);
-	ksprintf(&bufstr, "\t%f",val[2]);
+	ksprintf(&bufstr, "\t%f",lh3->lh3[s][i*3+0]);
+	ksprintf(&bufstr, "\t%f",lh3->lh3[s][i*3+1]);
+	ksprintf(&bufstr, "\t%f",lh3->lh3[s][i*3+2]);
       }
+
       if(bufstr.l!=0)
 	kputc('\n',&bufstr);
 

abcGL.h
@@ -32,7 +32,6 @@ private:
   void printLike(funkyPars *pars);
 
 public:
-
   void run(funkyPars  *pars);
   void print(funkyPars *pars);  
   void clean(funkyPars *pars);  

abcMajorMinor.h
@@ -1,4 +1,11 @@
 
+typedef struct{
+  char *hasAlloced; 
+  double **lh3; 
+}lh3struct;
+
+
+
 
 class abcMajorMinor:public abc{
   int doMajorMinor;
@@ -6,6 +13,8 @@ class abcMajorMinor:public abc{
   char *pest;
   int skipTriallelic;
   void majorMinorGL(funkyPars *pars,int doMajorMinor);
+  int doVcf;
+  int doGlf;
 public:
   void printArg(FILE *argFile);
   void run(funkyPars *pars);

commit c3a436ad73a8b0b5f4f7a8cb2c605583404a6943

Fixed bug that was introduced in  8e3a499ab1210e9448c163bcb3bf165d53bd9ec0 which caused beagle and vcf to malfunction if -domajorminor 3 was set

abcMajorMinor.cpp
@@ -130,7 +130,7 @@ abcMajorMinor::abcMajorMinor(const char *outfiles,argStruct *arguments,int input
   }
 
   getOptions(arguments);
-  if(doMajorMinor==0 || doMajorMinor ==3 )
+  if(doMajorMinor==0)
     shouldRun[index] =0;
 
   printArg(arguments->argumentFile);
@@ -347,41 +347,41 @@ void majorMinorCounts(suint **counts,int nFiles,int nSites,char *major,char *min
 
 
 void abcMajorMinor::run(funkyPars *pars){
-  if(doMajorMinor==0 || doMajorMinor ==3 )
+  if(doMajorMinor==0)
     return;
   if(doMajorMinor==1 && pars->likes==NULL){
     fprintf(stderr,"[%s.%s():%d] Problem here:\n",__FILE__,__FUNCTION__,__LINE__);
     exit(0);
   }
 
-  
+  if(doMajorMinor!=3){
   //allocate and initialize
-  pars->major = new char [pars->numSites];
-  pars->minor = new char [pars->numSites];
-  memset(pars->major,4,pars->numSites);
-  memset(pars->minor,4,pars->numSites);
-  
-  if(doMajorMinor!=2)
-    majorMinorGL(pars,doMajorMinor);
-  else if(doMajorMinor==2)
-    majorMinorCounts(pars->counts,pars->nInd,pars->numSites,pars->major,pars->minor,pars->keepSites,doMajorMinor,pars->ref,pars->anc);
-  else
-    fprintf(stderr,"[%s.%s()%d] Should never happen\n",__FILE__,__FUNCTION__,__LINE__);
-
-  if(doSaf!=0&&pest!=NULL&&skipTriallelic==1){
-    //fix case of triallelic site in pest output when doing pest
-    for(int s=0;s<pars->numSites;s++){
-      if(pars->keepSites[s]==0)
-	continue;
-      int a=refToInt[pars->anc[s]];
-      int b=refToInt[pars->major[s]];
-      int c=refToInt[pars->minor[s]];
-      if(a!=b&&a!=c)
-	pars->keepSites[s]=0;
+    pars->major = new char [pars->numSites];
+    pars->minor = new char [pars->numSites];
+    memset(pars->major,4,pars->numSites);
+    memset(pars->minor,4,pars->numSites);
+    
+    if(doMajorMinor!=2)
+      majorMinorGL(pars,doMajorMinor);
+    else if(doMajorMinor==2)
+      majorMinorCounts(pars->counts,pars->nInd,pars->numSites,pars->major,pars->minor,pars->keepSites,doMajorMinor,pars->ref,pars->anc);
+    else
+      fprintf(stderr,"[%s.%s()%d] Should never happen\n",__FILE__,__FUNCTION__,__LINE__);
+    
+    if(doSaf!=0&&pest!=NULL&&skipTriallelic==1){
+      //fix case of triallelic site in pest output when doing pest
+      for(int s=0;s<pars->numSites;s++){
+	if(pars->keepSites[s]==0)
+	  continue;
+	int a=refToInt[pars->anc[s]];
+	int b=refToInt[pars->major[s]];
+	int c=refToInt[pars->minor[s]];
+	if(a!=b&&a!=c)
+	  pars->keepSites[s]=0;
+      }
+      
     }
-
   }
-
   //if user has requested reference/ancestral then it is done in majorMinorGL and majorMinorCounts 0.585
   /*
   if(doMajorMinor==4||doMajorMinor==5)