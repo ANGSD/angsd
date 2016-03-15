a4f78f5c0cf433c8e5b2d22b89728c22bdb8a1b8
aalbrechtsen <andersdetermig@gmail.com>
2016-03-15 16:28:58 +0100

    changed filter of too low GL for -doAsso 1

--- analysisFunction.cpp
+++ analysisFunction.cpp
@@ -340,10 +340,11 @@ double **angsd::get3likesRMlow(funkyPars *pars,int *keepInd){
       loglike[s][count*3+2]=pars->likes[s][i*10+angsd::majorminor[pars->minor[s]][pars->minor[s]]];
       count++;
   
-      if(loglike[s][i*3+0] < -20 && loglike[s][i*3+1] < -20 && loglike[s][i*3+2] < -20){
-	loglike[s][i*3+0] = 0;
-	loglike[s][i*3+1] = 0;
-	loglike[s][i*3+2] = 0;
+      if(loglike[s][count*3+0] < -20 && loglike[s][count*3+1] < -20 && loglike[s][count*3+2] < -20){
+	loglike[s][count*3+0] = 0;
+	loglike[s][count*3+1] = 0;
+	loglike[s][count*3+2] = 0;
+
       }
     }
   }
