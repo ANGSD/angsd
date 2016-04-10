2b1f63541b6d5adfb676090abbe99357441c0c1d
aalbrechtsen <andersdetermig@gmail.com>
2016-04-10 17:22:03 +0200

    new version of HWE that allows for all types of diviations from HWE. Old version can still be used -doHWE_F which allows for inbreeding input from user. Output also changed slightly

--- abcHWE.cpp
+++ abcHWE.cpp
@@ -1,10 +1,16 @@
 /*
-  thorfinn thorfinn@binf.ku.dk 19dec 2012
 
-  Authors of this file
-  filipe , anders , thorfinn
+  test for HWE from genotype likelihhoods
 
-  part of angsd
+
+  Anders albrecht@binf.ku.dk 10 April 2016
+
+  Authors of this file:
+  Anders 
+
+  part of ANGSD: http://www.popgen.dk/angsd 
+
+  
 
  */
 #include <htslib/kstring.h>
@@ -19,7 +25,7 @@ void abcHWE::printArg(FILE *argFile){
 }
 
 void abcHWE::getOptions(argStruct *arguments){
-  int tmpDoMaf =0;
+
   int GL =0;
   HWE_pval = angsd::getArg("-HWE_pval",HWE_pval,arguments);
 
@@ -35,11 +41,7 @@ void abcHWE::getOptions(argStruct *arguments){
   if(doHWE==0)
     return;
   GL=angsd::getArg("-GL",GL,arguments);
-  tmpDoMaf=angsd::getArg("-doMaf",tmpDoMaf,arguments);
-  if(doHWE&&(tmpDoMaf==0)){
-    fprintf(stderr,"You supplied -HWE_pval, you should also choose -doMaf\n");
-    exit(0);
-  }
+
   if(arguments->inputtype==INPUT_BEAGLE||arguments->inputtype==INPUT_VCF_GP){
     fprintf(stderr,"Error: you cannot estimate HWE based on posterior probabilities \n");
     exit(0);
@@ -77,7 +79,7 @@ abcHWE::abcHWE(const char *outfiles,argStruct *arguments,int inputtype){
   if(doHWE>0){
     outfileZ = aio::openFileBG(outfiles,postfix);
     //print header
-    const char *str = "Chromo\tPosition\tMajor\tMinor\tFreq\thweFreq\tF\tLRT\tp-value\n";
+    const char *str = "Chromo\tPosition\tMajor\tMinor\thweFreq\tFreq\tF\tLRT\tp-value\n";
     aio::bgzf_write(outfileZ,str,strlen(str));
   }
 }
@@ -100,6 +102,7 @@ void abcHWE::clean(funkyPars *pars){
 
   funkyHWE *hweStruct =(funkyHWE *) pars->extras[index];
   delete[] hweStruct->freq;
+  delete[] hweStruct->freqHWE;
   delete[] hweStruct->F;
   delete[] hweStruct->like0;
   delete[] hweStruct->likeF;
@@ -116,7 +119,7 @@ void abcHWE::print(funkyPars *pars){
   for(int s=0;s<pars->numSites;s++){
     if(pars->keepSites[s]==0) 
       continue;
-    freqStruct *freq = (freqStruct *) pars->extras[6];
+   
     float lrt= 2*hweStruct->like0[s]-2*hweStruct->likeF[s];
     //DRAGON lrt is sometimes nan
     float pval;
@@ -127,7 +130,7 @@ void abcHWE::print(funkyPars *pars){
     else			
       pval=1-chisq->cdf(lrt);
     //    fprintf(stderr,"lrt:%f\n",lrt);
-    ksprintf(&bufstr,"%s\t%d\t%c\t%c\t%f\t%f\t%f\t%e\t%e\n",header->target_name[pars->refId],pars->posi[s]+1,intToRef[pars->major[s]],intToRef[pars->minor[s]],freq->freq[s],hweStruct->freq[s],hweStruct->F[s],lrt,pval);
+    ksprintf(&bufstr,"%s\t%d\t%c\t%c\t%f\t%f\t%f\t%e\t%e\n",header->target_name[pars->refId],pars->posi[s]+1,intToRef[pars->major[s]],intToRef[pars->minor[s]],hweStruct->freqHWE[s],hweStruct->freq[s],hweStruct->F[s],lrt,pval);
 
   }
   aio::bgzf_write(outfileZ,bufstr.s,bufstr.l);bufstr.l=0;
@@ -139,33 +142,45 @@ void abcHWE::run(funkyPars *pars){
   if(doHWE==0)
     return;
 
-  //  pars->hweStruct = new funkyHWE;//old
-  funkyHWE *hweStruct = new funkyHWE;//new
+  funkyHWE *hweStruct = new funkyHWE;
 
   double *freq = new double[pars->numSites];
+  double *freqHWE = new double[pars->numSites];
   double *F = new double[pars->numSites];
   double *like0 = new double[pars->numSites];
   double *likeF = new double[pars->numSites];
-  freqStruct *freqS = (freqStruct *) pars->extras[6];
-
+ 
   double **loglike3;
   loglike3=angsd::get3likesRescale(pars);
+
 
   for(int s=0;s<pars->numSites;s++){
     if(pars->keepSites[s]==0) 
       continue;
-    
+    //est under HWE
+    freqHWE[s] = angsd::estFreq(loglike3[s],pars->nInd);
+
+
+
     //start parameter
     double x[2];
-    x[0]=freqS->freq[s];
+    x[1]=0.0; //F
+    x[0]=freqHWE[s]; // freq
+
+    //log like for HWE freq
+    like0[s] = HWE_like(x,loglike3[s],pars->nInd);
+
+
+    //start parameter for EM_F
+    x[0]=freqHWE[s];
     x[1]=0.05;
+
     estHWE(x,loglike3[s],pars->nInd);
     freq[s]=x[0];
     F[s]=x[1];
     likeF[s] = HWE_like(x,loglike3[s],pars->nInd);
-    x[1]=0.0;
-    x[0]=freqS->freq[s];
-    like0[s] = HWE_like(x,loglike3[s],pars->nInd);
+
+
     //    fprintf(stderr,"%f\t%f\n",x[0],x[1]);
     //fprintf(stderr,"%f\t%f\t%f\n",loglike3[s][0],loglike3[s][1],loglike3[s][2]);
     float lrt= 2*like0[s]-2*likeF[s];
@@ -177,6 +192,7 @@ void abcHWE::run(funkyPars *pars){
 
 
   hweStruct->freq=freq;
+  hweStruct->freqHWE=freqHWE;
   hweStruct->F=F;
   hweStruct->like0=like0;
   hweStruct->likeF=likeF;
@@ -195,48 +211,79 @@ double abcHWE::differ(double *x,double *y){
 
 
 void abcHWE::HWE_EM(double *x,double *loglike,int nInd){
-  
-  double freq=x[0];
-  double F=x[1];
-  double p0=(pow(1-freq,2)+freq*(1-freq)*F);
-  double p1=(2*freq*(1-freq)-2*freq*(1-freq)*F);
-  double p2=(pow(freq,2)+freq*(1-freq)*F);
+  // freq2HWE <- function(x) {  freq <- x[2]*0.5 + x[1];   F <-	1 - x[2]/(2*freq*(1-freq)); c(freq,F);}
 
-  double freq2=0;
-  double F2=0;
+
+  
+
+  double freq0,freq1,freq2;
+  if(1){ // use F formulation instead of p0,p1,p2: compatable with the likelihood function
+    double freq = x[0];
+    double Fadj = freq*(1-freq)*x[1];
+    freq0= pow(1-freq,2) + Fadj;
+    freq1= 2*freq*(1-freq) - 2*Fadj;
+    freq2= pow(freq,2) + Fadj;
+  }
+  else{
+    freq2 = x[0];
+    freq1 = x[1];
+    freq0 = 1- x[0] - x[1];
+  }
+ 
+  double newFreq0=0;
+  double newFreq1=0;
+  double newFreq2=0;
   double norm;
-  double oHO=0;
-  double eHO=nInd*(pow(freq,2)+pow(1-freq,2));
 
   for(int i=0;i<nInd;i++){
-    norm=angsd::addProtect3(log(p0)+loglike[i*3+0],log(p1)+loglike[i*3+1],log(p2)+loglike[i*3+2]);
-    freq2+=exp(log(p1)+loglike[i*3+1]-norm)+exp(log(2)+log(p2)+loglike[i*3+2]-norm);
-    oHO+=exp(log(p0)+loglike[i*3+0]-norm)+exp(log(p2)+loglike[i*3+2]-norm);
-  //oHO+=(p0*loglike[i*3+0]+p2*loglike[i*3+2])/(2*norm);
-    //fprintf(stderr,"1 %f 2 %f 3 %f\n",loglike[i*3+0],loglike[i*3+1],loglike[i*3+2]);
+    norm=angsd::addProtect3(log(freq0)+loglike[i*3+0],log(freq1)+loglike[i*3+1],log(freq2)+loglike[i*3+2]);
+    newFreq0 += exp(log(freq0)+loglike[i*3+0] - norm);
+    newFreq1 += exp(log(freq1)+loglike[i*3+1] - norm);
+    newFreq2 += exp(log(freq2)+loglike[i*3+2] - norm);
   }
-  F2=(oHO-eHO)/(nInd-eHO);
-  if(F2<0)
-    F2=0;
-  if(F2>1)
-    F2=1;
-  
-  freq2=freq2/(2*nInd);
-  //fprintf(stderr,"norm %f\tx[0] %f\tx[1] %f\t1 %f 2 %f 3 %f oHO %f eHO %f freq %f\n",norm,x[0],x[1],p0,p1,p2,oHO,eHO,freq2);
 
-  if(freq2<0.0001)
-    F2=0;
-  //    F2=0;
-  x[0]=freq2;
-  x[1]=F2;
-  if(freq2>1.0000001){
-    fprintf(stderr,"something is wrong i HWE\t freq %f\n",freq2);
+  newFreq0 = newFreq0/nInd;
+  newFreq1 = newFreq1/nInd;
+  newFreq2 = newFreq2/nInd;
+
+  if(newFreq0 < 0.0001)
+    newFreq0 = 0.0001;
+
+  if(newFreq1 < 0.0001)
+    newFreq1 = 0.0001;
+
+  if(newFreq2 < 0.0001)
+    newFreq2 = 0.0001;
+
+  norm = newFreq0 + newFreq1 + newFreq2;
+  newFreq0 = newFreq0/norm;
+  newFreq1 = newFreq1/norm;
+  newFreq2 = newFreq2/norm;
+
+  
+  if(newFreq0 + newFreq1 >1.00001){
+    fprintf(stderr,"something is wrong i HWE\t freq %f\t F %f\n",x[0],x[1]);
     fflush(stderr);
     exit(0);
-
   }
-}
+ 
+ 
+  if(1){
+    if(isnan(newFreq1*0.5 + newFreq2)){
+      fprintf(stderr,"problems in abcHWE: \tx[0] %f\tx[1] %f\told 1 %f 2 %f 3 %f\tnew 1 %f 2 %f 3 %f \n",x[0],x[1],freq0,freq1,freq2,newFreq0,newFreq1,newFreq2);
+      //      exit(0);
+    }
+    x[0] = newFreq1*0.5 + newFreq2;
+    x[1] = 1 - newFreq1/(2*x[0]*(1-x[0])) ;
+  }
+  else{
+    x[0] = newFreq2;
+    x[1] = newFreq1;
+  }
 
+
+ 
+}
 
 
 
@@ -254,51 +301,22 @@ double abcHWE::HWE_like(double *x,double *loglike,int nInd){
 
 void abcHWE::estHWE(double *x,double *loglike,int nInd){
 
-  double l,d;
-  if(testMe){
-    l=HWE_like(x,loglike,nInd);
-    d=l;
-  }
   double y[2];// = new double[2];
   y[0] = x[0]+2;
   y[1] = x[1]+2;
   int iter=50;
+  double dif;
   //fprintf(stderr,"%f\n",differ(x,y));
   int printer=0;
   for(int i=0;i<iter;i++){
     HWE_EM(x,loglike,nInd);
-    if(testMe){
-      l=HWE_like(x,loglike,nInd);
-      if(d>l+0.01){
-	//   fprintf(stderr,"d %f\tl %f\n",d,l);
-	printer=1;
-      }
-      d=l;
-    }
-    double dif = differ(x,y);
+    dif = differ(x,y);
     // fprintf(stderr,"%f\n",dif);
     if(dif<tolStop)
       break;
     y[0] = x[0];
     y[1] = x[1];
 
-    
   }
-  if(printer && testMe ){
-    x[0]=0.05;
-    x[1]=0.05;
-    l=HWE_like(x,loglike,nInd);
-    for(int i=0;i<iter;i++){
-      fprintf(stderr,"like %d\t%f\tf %f\tF %f\t%d\n",i,l,x[0],x[1],nInd);
-      HWE_EM(x,loglike,nInd);
-      l=HWE_like(x,loglike,nInd);
-    }
 
-    for(int ind=0;ind<nInd;ind++){
-      fprintf(stdout,"likes %f, %f, %f\n",loglike[ind*3+0],loglike[ind*3+1],loglike[ind*3+2]);
-    }
-    fflush(stderr);
-    exit(0);
-
-  }
 }
