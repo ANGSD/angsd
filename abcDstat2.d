75d9f7c1e80ad820bd19e43f1a1e202a471e7147
ANGSD <thorfinn.sand@gmail.com>
2016-07-07 12:15:42 +0200

    Now moving to new naming scheme. odd numbers exist on github , even corresponds to a single github version and the tar.gz on the popgen.dk/angsd

--- abcDstat2.d
+++
@@ -1,9 +0,0 @@
-abcDstat2.o: abcDstat2.cpp shared.h bambi_interface.h argStruct.h \
- /pontus/data/samuele/sim/testJuly2016/htslib/htslib/hts.h \
- /pontus/data/samuele/sim/testJuly2016/htslib/htslib/hts_defs.h \
- /pontus/data/samuele/sim/testJuly2016/htslib/htslib/sam.h \
- /pontus/data/samuele/sim/testJuly2016/htslib/htslib/hts.h \
- analysisFunction.h chisquare.h \
- /pontus/data/samuele/sim/testJuly2016/htslib/htslib/bgzf.h \
- /pontus/data/samuele/sim/testJuly2016/htslib/htslib/kstring.h abc.h \
- abcDstat2.h
