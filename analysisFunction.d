8a37cac641c0bb83425b8eb213a4c217ba7cb9d6
samuele soraggi <samuele@randy>
2017-02-06 10:34:10 +0100

    added matrix containing integers

---
+++ analysisFunction.d
@@ -0,0 +1,8 @@
+analysisFunction.o: analysisFunction.cpp analysisFunction.h shared.h \
+ bambi_interface.h argStruct.h \
+ /emc/samuele/DstatWork/angsdVersions/angsdSamuele20170126/htslib/htslib/hts.h \
+ /emc/samuele/DstatWork/angsdVersions/angsdSamuele20170126/htslib/htslib/hts_defs.h \
+ /emc/samuele/DstatWork/angsdVersions/angsdSamuele20170126/htslib/htslib/sam.h \
+ /emc/samuele/DstatWork/angsdVersions/angsdSamuele20170126/htslib/htslib/hts.h \
+ chisquare.h \
+ /emc/samuele/DstatWork/angsdVersions/angsdSamuele20170126/htslib/htslib/bgzf.h
