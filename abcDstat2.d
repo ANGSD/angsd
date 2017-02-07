3f9751cc9416caa51d41f96f01904604b562a66e
samuele soraggi <samuele@rowdy>
2017-02-07 13:44:04 +0100

    removed accidentally added .d files

--- abcDstat2.d
+++
@@ -1,9 +0,0 @@
-abcDstat2.o: abcDstat2.cpp shared.h bambi_interface.h argStruct.h \
- /emc/samuele/DstatWork/angsdVersions/angsdSamuele20170126/htslib/htslib/hts.h \
- /emc/samuele/DstatWork/angsdVersions/angsdSamuele20170126/htslib/htslib/hts_defs.h \
- /emc/samuele/DstatWork/angsdVersions/angsdSamuele20170126/htslib/htslib/sam.h \
- /emc/samuele/DstatWork/angsdVersions/angsdSamuele20170126/htslib/htslib/hts.h \
- analysisFunction.h chisquare.h \
- /emc/samuele/DstatWork/angsdVersions/angsdSamuele20170126/htslib/htslib/bgzf.h \
- /emc/samuele/DstatWork/angsdVersions/angsdSamuele20170126/htslib/htslib/kstring.h \
- abc.h abcDstat2.h
