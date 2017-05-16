04a8409283480cd2779cc1239b6d9a93bc90add1
ANGSD <thorfinn.sand@gmail.com>
2017-05-16 17:23:15 +0800

    Some updates for the scounts

--- misc/scounts.cpp
+++ misc/scounts.cpp
@@ -4,7 +4,8 @@
 #include <cstdio>
 #include <cassert>
 #include <zlib.h>
-
+#include <vector>
+#include <map>
 
 typedef unsigned char uchar;
 
@@ -51,6 +52,7 @@ FILE *myfopen(const char *file,char *mode){
   }
   return fp;
 }
+
 gzFile mygzopen(const char *file,char *mode){
   gzFile fp = Z_NULL;
   fp = gzopen(file,mode);
@@ -61,58 +63,72 @@ gzFile mygzopen(const char *file,char *mode){
   return fp;
 }
 
+typedef std::map<int,int*> smap;
+
+
+void counter(smap &sm,char *fname){
+  gzFile dat = mygzopen(fname,(char*)"rb");
+  counts cnts;
+  while(gzread(dat,&cnts,sizeof(counts))){
+    smap::iterator it=sm.find(cnts.rel_pos);
+    
+    if(it==sm.end()){
+      int *tmp = new int[4];
+      tmp[0]=tmp[1]=tmp[2]=tmp[3]=0;
+      sm[cnts.rel_pos] = tmp;
+      it = sm.find(cnts.rel_pos);
+    }
+    int *total = it->second;
+    int tmp[4] = {cnts.A,cnts.C,cnts.G,cnts.T};
+    int tmp2[4] = {0,0,0,0};
+    int at=0;
+    for(int i=0;i<4;i++)
+      if(tmp[i]>0)
+	tmp2[at++] =i;
+    //    fprintf(stderr,"informative sites:%d\n",at);
+    int which = lrand48() % at;
+    it->second[tmp2[which]]++;
+  }
+}
 
 int main(int argc,char **argv){
-  
+  long seed =0;
   unsigned nsites = 70e6;
   
   if(argc==1){
-    fprintf(stderr,"./a.out -c \n./a.out -d \n");
+    fprintf(stderr,"./a.out filelist population \n");
     return 0;
   }
-  char *c=argv[1];
-  if(!strcasecmp(c,"-c")){
-    char *keep = new char[nsites];
-    keep=(char*) memset(keep,0,nsites*sizeof(char));
-    FILE *dat = myfopen("data4.txt",(char*)"wb");
-    fprintf(stderr,"encrypying\n");
-    int nkeep=0;
-    for(int i=0;i<nsites;i++){
-      counts cnt={0,0,0,0};
-      int val =0;
-      double r=drand48();
-      if(r<=0.06){
-	cnt.rel_pos = i;
-	//	fprintf(stderr,"hit at:%d\n",i);
-	nkeep++;
-        int which =lrand48()%4;
-	for(int i=0;i<4;i++){
-	  int val = 0;
-	  if(i==which){
-	    
-	    val++;
-	  }if(drand48()>0.9999)
-	    val++;
-	  set(cnt,i,val);
-	}
-	val=1;
-	//	print_byte((uint8_t,cnt);
-	fwrite(&cnt,sizeof(counts),1,dat);
-	keep[i] = 1;
-      }
-      //printit(cnt,stdout);
-      //      fprintf(stderr,"val si:%d\n",val);
-    }
-    fprintf(stderr,"nkeep:%d\n",nkeep);
-    fclose(dat);
-  }else if(!strcasecmp(c,"-d")){
-    assert(argc ==3);
+  if(0==strcmp(argv[1],"print")){
+    fprintf(stderr,"printing\n");
     gzFile dat = mygzopen(argv[2],(char*)"rb");
     counts cnts;
-    while(gzread(dat,&cnts,sizeof(counts))){
+    while(gzread(dat,&cnts,sizeof(counts)))
       printit(cnts,stdout);
-
+    return 0;
+    fprintf(stderr,"done printing\n");
+  }
+  
+  if(argc==4){
+    fprintf(stderr,"\t-> Setting seed: \n");
+    seed = atol(argv[3]);
+  }
+  srand48(seed);
+  char *c=argv[1];
+  smap sm;
+  gzFile gz = mygzopen(argv[1],(char*)"rb");
+  char buf[4096];
+  
+  while(gzgets(gz,buf,4096)){
+    char *file = strtok(buf," \t");
+    char *pop = strtok(NULL," \t");
+    if(strcmp(pop,argv[2])){
+      counter(sm,file);
     }
+  }
+  for(smap::iterator it=sm.begin();it!=sm.end();it++){
+    int *ary = it->second;
+    fprintf(stdout,"%d\t%d\t%d\t%d\t%d\n",it->first,ary[0],ary[1],ary[2],ary[3]);
   }
   return 0;
 }
