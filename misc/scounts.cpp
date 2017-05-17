f8b7a9299c4cc88db971cc493b7bea27fdec2174
ANGSD <thorfinn.sand@gmail.com>
2017-05-17 12:04:43 +0800

    Final edits 2

--- misc/scounts.cpp
+++ misc/scounts.cpp
@@ -92,17 +92,6 @@ void set(counts &cnt,int type,int val){
 void printit(counts &cnt,FILE *fp){
   fprintf(stdout,"%u\t%d\t%d\t%d\t%d\n",cnt.rel_pos,cnt.A,cnt.C,cnt.G,cnt.T);
 }
-const char *bit_rep[16] = {
-    [ 0] = "0000", [ 1] = "0001", [ 2] = "0010", [ 3] = "0011",
-    [ 4] = "0100", [ 5] = "0101", [ 6] = "0110", [ 7] = "0111",
-    [ 8] = "1000", [ 9] = "1001", [10] = "1010", [11] = "1011",
-    [12] = "1100", [13] = "1101", [14] = "1110", [15] = "1111",
-};
-
-void print_byte(uint8_t byte)
-{
-  fprintf(stderr,"%s%s\n", bit_rep[byte >> 4], bit_rep[byte & 0x0F]);
-}
 
 FILE *myfopen(const char *file,char *mode){
   FILE *fp = NULL;
