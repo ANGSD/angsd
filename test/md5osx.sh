25c235448d534f957b15c5e8d15f31a5a5c54c89
ANGSD <thorfinn.sand@gmail.com>
2017-06-16 10:51:10 +0100

    Added small bash script for emulating coreutils md5 -c behavior

---
+++ test/md5osx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+while read storedhash file
+do
+    generatedhash=$(md5 -q "$file")
+    if [[ $generatedhash != $storedhash ]]; then
+	echo "Hash for file '$file 'does not match"
+	exit 1
+    fi
+done <$2
+
