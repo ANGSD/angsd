37e4e16f4666c0d66830a0705532eb03f3283790
ANGSD <thorfinn.sand@gmail.com>
2019-02-15 00:31:19 +0100

    more edits

--- vcfReader2.cpp
+++ vcfReader2.cpp
@@ -24,7 +24,62 @@ typedef struct satan_t{
   int nind;
 }satan;
 
-std::vector<satan> jobs;
+typedef struct{
+  char *fname;
+  char *seek;
+  htsFile *hts_file;
+  bcf_hdr_t *hdr;
+  hts_idx_t *idx;
+  hts_itr_t *iter;
+  int nsamples;
+}htsstuff;
+
+void htsstuff_seek(htsstuff *hs,char *seek){
+  assert(seek);
+  if(seek){
+    fprintf(stderr,"\t-> Setting iterator to: %s\n",seek);fflush(stderr);
+    hs->idx=bcf_index_load(hs->fname);
+    hs->iter=bcf_itr_querys(hs->idx,hs->hdr,seek);
+  }
+}
+
+htsstuff *htsstuff_init(char *fname,char *seek){
+  fprintf(stderr,"[%s] seek:%s\n",__FUNCTION__,seek);
+  htsstuff *hs = new htsstuff;
+  hs->fname=NULL;hs->fname=strdup(fname);
+  hs->seek=NULL;
+  if(seek) hs->seek=strdup(seek);
+  hs->hts_file=NULL;
+  hs->hdr=NULL;
+  hs->idx=NULL;
+  hs->iter=NULL;
+  hs->nsamples = 0;
+  hs->hts_file=hts_open(hs->fname,"r");
+  assert(hs->hts_file);
+  
+  hs->hdr = bcf_hdr_read(hs->hts_file);
+  assert(hs->hdr);
+
+  hs->nsamples = bcf_hdr_nsamples(hs->hdr);
+  if(hs->seek)
+    htsstuff_seek(hs,hs->seek);
+  return hs;
+}
+
+void htsstuff_destroy(htsstuff *hs){
+  free(hs->seek);
+  
+  if(hs->hdr)
+    bcf_hdr_destroy(hs->hdr);
+  if(hs->hts_file)
+    bcf_close(hs->hts_file);
+
+  if(hs->iter)
+    hts_itr_destroy(hs->iter);
+  if(hs->idx)
+    hts_idx_destroy(hs->idx);
+
+}
 
 
 // https://github.com/samtools/htslib/blob/57fe419344cb03e2ea46315443abd242489c32f2/vcf.c#L53
@@ -114,57 +169,36 @@ void buildreorder(int swap[10],char **alleles,int len){
 #endif
 }
 
-size_t getgls(char*fname,std::vector<double *> &mygl, std::string &vcf_format_field, std::string &vcf_allele_field,char *seek){
+funkyPars *myfetch(htsstuff *hs,int chunksize){
+  std::string vcf_format_field = "PL";
   for(int i=0;i<PHREDMAX;i++){    
     pl2ln[i] = log(pow(10.0,-0.1*i));
   }
-  htsFile * inf = NULL;inf=hts_open(fname, "r");assert(inf);
-  bcf_hdr_t *hdr = bcf_hdr_read(inf);
   bcf1_t *rec = NULL;rec=bcf_init();assert(rec);
-  hts_idx_t *idx=NULL;
-  hts_itr_t *iter=NULL;
 
-  if(seek){
-    fprintf(stderr,"\t-> Setting iterator to: %s\n",seek);fflush(stderr);
-    idx=bcf_index_load(fname);
-    iter=bcf_itr_querys(idx,hdr,seek);
-  }
   //   http://wresch.github.io/2014/11/18/process-vcf-file-with-htslib.html
   // counters
   int n    = 0;  // total number of records in file
   int nsnp = 0;  // number of SNP records in file
   int nseq = 0;  // number of sequences
-  int nsamples = 0;
-
+ 
   // pl data for each call
   int npl_arr = 0;
   int npl     = 0;
   int32_t *pl = NULL;
 
-   // gt data for each call
-  int32_t ngt_arr = 0;
-  int32_t ngt     = 0;
-  int32_t *gt     = NULL;
-
-  
-  // af1/af data for each call
-  int naf_arr = 0;
-  int naf     = 0;
-  float *af     = NULL;
-
-  // read header
-  nsamples = bcf_hdr_nsamples(hdr);
-  //  fprintf(stderr, "\t-> File %s contains %i samples\n", fname, nsamples);
+#ifdef __WITH_MAIN__
   const char **seqnames = NULL;
-  seqnames = bcf_hdr_seqnames(hdr, &nseq); assert(seqnames);//bcf_hdr_id2name(hdr,i)
-
+  seqnames = bcf_hdr_seqnames(hs->hdr, &nseq); assert(seqnames);//bcf_hdr_id2name(hdr,i)
+#endif
+  
   char *chr;
   while(1){
-    if(seek==NULL){
-      if(bcf_read(inf,hdr,rec)!=0)	
+    if(hs->iter==NULL){
+      if(bcf_read(hs->hts_file,hs->hdr,rec)!=0)	
 	break;
     }else{
-      if(bcf_itr_next(inf, iter, rec)!=0)
+      if(bcf_itr_next(hs->hts_file, hs->iter, rec)!=0)
 	break;
     }
     n++;
@@ -181,15 +215,15 @@ size_t getgls(char*fname,std::vector<double *> &mygl, std::string &vcf_format_fi
     buildreorder(myreorder,rec->d.allele,rec->n_allele);
     
 
-    double dupergl[10*nsamples]; 
+    double dupergl[10*hs->nsamples]; 
 
     if(vcf_format_field == "PL") {
-      npl = bcf_get_format_int32(hdr, rec, "PL", &pl, &npl_arr);
+      npl = bcf_get_format_int32(hs->hdr, rec, "PL", &pl, &npl_arr);
       float ln_gl[npl];
       if(npl<0){
         // return codes: https://github.com/samtools/htslib/blob/bcf9bff178f81c9c1cf3a052aeb6cbe32fe5fdcc/htslib/vcf.h#L667
         // no PL tag is available
-        fprintf(stderr, "BAD SITE %s:%d. return code:%d while fetching PL tag\n", bcf_seqname(hdr,rec), rec->pos, npl);
+        fprintf(stderr, "BAD SITE %s:%d. return code:%d while fetching PL tag\n", bcf_seqname(hs->hdr,rec), rec->pos, npl);
         continue;
       }
       // https://github.com/samtools/bcftools/blob/e9c08eb38d1dcb2b2d95a8241933daa1dd3204e5/plugins/tag2tag.c#L151
@@ -205,7 +239,7 @@ size_t getgls(char*fname,std::vector<double *> &mygl, std::string &vcf_format_fi
 	//         fprintf(stderr, "%d %f\n", pl[i], ln_gl[i]);
       }
 
-      for(int ind=0;ind<nsamples;ind++){
+      for(int ind=0;ind<hs->nsamples;ind++){
 	for(int o=0;o<10;o++)
 	  dupergl[ind*10+o] = ln_gl[myreorder[o]]; 
       }
@@ -216,8 +250,8 @@ size_t getgls(char*fname,std::vector<double *> &mygl, std::string &vcf_format_fi
     //    fprintf(stderr,"keepind:%d\n",keepInd);
 
     
-    naf = bcf_get_info_float(hdr, rec, vcf_allele_field.c_str(), &af, &naf_arr);
-    fprintf(stderr,"rec->pos:%d npl:%d naf:%d rec->n_allele:%d\n",rec->pos,npl,naf,rec->n_allele);    
+    //naf = bcf_get_info_float(hdr, rec, vcf_allele_field.c_str(), &af, &naf_arr);
+    //    fprintf(stderr,"rec->pos:%d npl:%d naf:%d rec->n_allele:%d\n",rec->pos,npl,naf,rec->n_allele);    
     //if multiple alt alleles then n_allele>3. We only care about diallelic ref/alt alleless
     //		if(rec->n_allele==4) fprintf(stdout,"\n%s\n",rec->d.allele[2]);
     //ok this is a bit messed up. apparantly sometime the allele is <*> sometimes not.
@@ -241,68 +275,15 @@ size_t getgls(char*fname,std::vector<double *> &mygl, std::string &vcf_format_fi
 #endif
       
   }
-  fprintf(stderr, "\t-> [file=\'%s\'][chr=\'%s\'] Read %i records %i of which were SNPs number of sites with data:%lu\n",fname,seek, n, nsnp,mygl.size()); 
+  //  fprintf(stderr, "\t-> [file=\'%s\'][chr=\'%s\'] Read %i records %i of which were SNPs number of sites with data:%lu\n",fname,seek, n, nsnp,mygl.size()); 
 
   free(pl);
-  free(gt);
-  bcf_hdr_destroy(hdr);
-  bcf_close(inf);
+
   bcf_destroy(rec);
 
-  if(iter)
-    hts_itr_destroy(iter);
-  if(idx)
-    hts_idx_destroy(idx);
-
-  //for(int i=0;i<nseq;i++)
-    free(seqnames);
-
-  return nsamples;
- 
-}
-
-void *wrap(void *ptr){
-  satan *god = (satan*) ptr;
-  god->nind=getgls(god->fname, god->mygl,god->vcf_format_field,god->vcf_allele_field,god->seek);
-  //  pthread_exit(NULL);//this is sometimes called without thread
   return NULL;
-}
-
-double ** readbcfvcf(char*fname,int &nind, std::string vcf_format_field, std::string vcf_allele_field,char *seek){
-  fprintf(stderr,"\t-> readbcfvcf seek:%s nind:%d\n",seek,nind);
-  htsFile * inf = NULL;inf=hts_open(fname, "r");assert(inf);  
-  bcf_hdr_t *hdr = NULL;hdr=bcf_hdr_read(inf);assert(hdr);
-  int isbcf=0;
-
-  satan god;
-  god.fname=fname;
-  god.vcf_format_field=vcf_format_field;
-  god.vcf_allele_field=vcf_allele_field;
-  god.seek=seek;
-  
-
-  if(inf->format.format==bcf){
-    isbcf=1;
-  }if(seek&&isbcf==0){
-    fprintf(stderr,"\t-> if choosing region then input file has to be bcf\n");
-    exit(0);
-  }
  
-  double **gls=NULL;
-  wrap(&god);
-  nind=god.nind;
-  gls=new double *[god.mygl.size()];
-  for(int i=0;i<god.mygl.size();i++)
-    gls[i] = god.mygl[i];
-
-  fprintf(stderr,"Done reading everything we have nsites:%lu for samples:%d\n",god.mygl.size(),nind);
-  //merge results
-  
-  if(hdr) bcf_hdr_destroy(hdr);
-  hts_close(inf);
-  return gls;
 }
-
 
 #ifdef __WITH_MAIN__
 int main(int argc, char **argv) {
@@ -316,8 +297,12 @@ int main(int argc, char **argv) {
   if(argc==3)
     reg=strdup(argv[2]);
   fprintf(stderr,"reg:%s\n",reg);
+
+
+  htsstuff *hs=htsstuff_init(argv[1],reg);
+
   int nind;
-  gls = readbcfvcf(argv[1],nind,pl,fr,reg);
+  funkyPars *fp = myfetch(hs,100);
   return 0;
 }
 
