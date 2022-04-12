09ef3d167adfdfb7f276ff9da4e889ab1ae93d27
Isin Altinkaya <isinaltinkaya@gmail.com>
2022-04-12 17:43:20 +0200

    [feat](amova): Add 2 ind pairwise genotype comparison 3x3 matrix

--- abcAMOVA.cpp
+++ abcAMOVA.cpp
@@ -2,6 +2,10 @@
 
 abcAMOVA: Analysis of Molecular Variance
 
+
+notes:
+-site should be diallelic
+
 */
 
 #include <cassert>
@@ -12,126 +16,179 @@ abcAMOVA: Analysis of Molecular Variance
 #include "aio.h"
 #include "shared.h" 
 #include "analysisFunction.h" 
+#include "abcCallGenotypes.h"
 
 #include "abcAMOVA.h"
+#include "abcHaploCall.h"
 
 
 
 void abcAMOVA::printArg(FILE *argFile){
-	fprintf(argFile,"------------------------\n%s:\n",__FILE__);
-	fprintf(argFile,"-doAMOVA\t%d\n",doAMOVA);
-	fprintf(argFile,"\t\t1: init\n");
+    fprintf(argFile,"------------------------\n%s:\n",__FILE__);
+    fprintf(argFile,"-doAMOVA\t%d\n",doAMOVA);
+    fprintf(argFile,"\t\t1: init\n");
 }
 
-void abcAMOVA::getOptions(argStruct *arguments){
+void abcAMOVA::getOptions(argStruct *args){
 
-	doAMOVA=angsd::getArg("-doAMOVA",doAMOVA,arguments);
+    doAMOVA=angsd::getArg("-doAMOVA",doAMOVA,args);
 
-	if(doAMOVA==0){
-		shouldRun[index]=0;
-		return;
-	}
+    if(doAMOVA==0){
+        shouldRun[index]=0;
+        return;
+    }
 
-	if (doAMOVA == 1){
-		int doMajorMinor = 0;
-		doMajorMinor = angsd::getArg("-doMajorMinor",doMajorMinor,arguments);
+    if (doAMOVA == 1){
 
-		if(doMajorMinor!=1){
-			fprintf(stderr,"\t->\t-doAMOVA requires -doMajorMinor 1\n");
-			exit(0);
-		}
 
-		int GL = 0;
-		GL = angsd::getArg("-GL",GL,arguments);
+        int doMajorMinor = 0;
+        doMajorMinor = angsd::getArg("-doMajorMinor",doMajorMinor,args);
 
-		if(GL!=1){
-			fprintf(stderr,"\t->\t-doAMOVA requires -GL 1\n");
-			exit(0);
-		}
-	}else{
-		fprintf(stderr,"\t->\tuse -doAMOVA 1\n");
-		exit(0);
-	}
+        int doGeno=0;
+        doGeno = angsd::getArg("-doGeno",doGeno,args);
+        if(doGeno!=2){
+            fprintf(stderr,"\t->\t-doAMOVA %d requires -doGeno 2\n",doAMOVA);
+            exit(0);
+        }
+
+
+        //based on allele counts
+        if(doMajorMinor!=2){
+            fprintf(stderr,"\t->\t-doAMOVA %d requires -doMajorMinor 2\n",doAMOVA);
+            exit(0);
+        }
+
+        int doCounts=0;
+        doCounts=angsd::getArg("-doCounts",doCounts,args);
+
+        if(doCounts!=1){
+            fprintf(stderr,"\t->\t-doAMOVA %d requires -doCounts 1\n",doAMOVA);
+            exit(0);
+        }
+
+    }else if (doAMOVA == 2){
+
+        int doMajorMinor = 0;
+        doMajorMinor = angsd::getArg("-doMajorMinor",doMajorMinor,args);
+
+        if(doMajorMinor!=1){
+            fprintf(stderr,"\t->\t-doAMOVA %d requires -doMajorMinor 1\n",doAMOVA);
+            exit(0);
+        }
+
+        int GL = 0;
+        GL = angsd::getArg("-GL",GL,args);
+
+        if(GL!=1){
+            fprintf(stderr,"\t->\t-doAMOVA %d requires -GL 1\n",doAMOVA);
+            exit(0);
+        }
+
+    }else{
+        fprintf(stderr,"\t->\tuse -doAMOVA 1 or 2\n");
+        exit(0);
+    }
 
 
 }
 
 //constructor
 abcAMOVA::abcAMOVA(const char *outfiles,argStruct *arguments,int inputtype){
-	doAMOVA = 0;
-	refName = NULL;
-	outfile = NULL;
+    doAMOVA = 0;
+    refName = NULL;
+    outfile = NULL;
 
-	int doMajorMinor = 0;
-	int GL = 0;
 
-	char *empty;
-	//like_calc = new phys_genolike_calc( empty );
 
-	if (arguments->argc==2){
-		if(!strcasecmp(arguments->argv[1],"-doAMOVA")){
-			printArg(stdout);
-			exit(0);
-		} else
-			return;
-	}
+    if (arguments->argc==2){
+        if(!strcasecmp(arguments->argv[1],"-doAMOVA")){
+            printArg(stdout);
+            exit(0);
+        } else
+            return;
+    }
 
-	getOptions(arguments);
+    getOptions(arguments);
 
-	if(doAMOVA==0){
-		shouldRun[index] =0;
-		return ;
-	}
+    if(doAMOVA==0){
+        shouldRun[index] =0;
+        return ;
+    }
 
-	printArg(arguments->argumentFile);
-	outfile = aio::openFile(outfiles,".amovaout");
-	fprintf(outfile,"chr\tpos\tmajor\tminor\tlikes");
+    printArg(arguments->argumentFile);
+    outfile = aio::openFile(outfiles,".amovaout");
 
 }
 
 
 //destructor
 abcAMOVA::~abcAMOVA(){
-	if(doAMOVA==0)
-		return;
+    if(doAMOVA==0)
+        return;
 
-	if(outfile!=NULL) 
-		fclose(outfile);
+    if(outfile!=NULL) 
+        fclose(outfile);
 }
 
 
 
 void abcAMOVA::print(funkyPars *pars){
 
-	if(doAMOVA==1){
+    /*
+     * 0 = MM
+     * 1 = Mm
+     * 2 = mm
+     *
+     * M[i][j]
+     * number of sites where ind1=i and ind2=j
+     */
+    int pairwiseGenotypeMatrix[3][3] = {{0}};
 
-		chunkyT *chk = pars->chk;
+    if(doAMOVA==1){
+        genoCalls *geno =(genoCalls *) pars->extras[11];
 
-		for(int s=0;s<pars->numSites;s++){
+        for(int s=0;s<pars->numSites;s++){
 
-			for(int i=0;i<pars->nInd;i++){
+            for(int i=0;i<pars->nInd;i++){
 
-				assert(pars->likes!=NULL);
+                //fprintf(stderr,"%d\t",geno->dat[s][i]);
 
-				//print [chr pos major minor likes]
-				fprintf(outfile,"\n%s %d %c %c %d\n",header->target_name[pars->refId],pars->posi[s]+1,intToRef[pars->major[s]],intToRef[pars->minor[s]],pars->likes[s]);
-				fprintf(stderr,"\n%s %d %c %c %d\n",header->target_name[pars->refId],pars->posi[s]+1,intToRef[pars->major[s]],intToRef[pars->minor[s]],pars->likes[s]);
+                //print actual haplotypes
+                //if(geno->dat[s][i]==0)
+                //fprintf(stderr,"%c%c\n",intToRef[pars->major[s]],intToRef[pars->major[s]]);
+                //else if(geno->dat[s][i]==1)
+                //fprintf(stderr,"%c%c\n",intToRef[pars->major[s]],intToRef[pars->minor[s]]);
+                //else if(geno->dat[s][i]==2)
+                //fprintf(stderr,"%c%c\n",intToRef[pars->minor[s]],intToRef[pars->minor[s]]);
+                //else if(geno->dat[s][i]==-1)
+                //fprintf(stderr,"NN\n");
 
-				tNode *nd = chk->nd[s][i];
+            }
+            //fprintf(stderr,"%d %d\t",geno->dat[s][0],geno->dat[s][1]);
 
-				//for ind_i i, loop through all alignments at pos site_i s
-				for(int l=0;l<nd->l;l++){
-					//base_i
-					char c = nd->seq[l];
-				}
-			}
-		}
-	}
+            //ind0 ind1
+            pairwiseGenotypeMatrix[geno->dat[s][0]][geno->dat[s][1]]++;
+        }
+
+        //fprintf(stderr,"\n\n");
+        for(int gi=0; gi<3;gi++){
+            for (int gj=0; gj<3; gj++){
+                //fprintf(stderr,"%d\t",pairwiseGenotypeMatrix[gi][gj]);
+                fprintf(outfile,"%d\t",pairwiseGenotypeMatrix[gi][gj]);
+
+            }
+            //fprintf(stderr,"\n");
+            fprintf(outfile,"\n");
+        }
+    }
 }
 
+
+
 void abcAMOVA::run(funkyPars *pars){
-	if(doAMOVA==0)
-		return;
+
+    if(doAMOVA==0)
+        return;
 }
 
 void abcAMOVA::clean(funkyPars *pars){
--- abcAMOVA.h
+++ abcAMOVA.h
@@ -1,20 +1,44 @@
 #pragma once
 #include "abc.h"
 
+//typedef struct{
+  //int **dat;
+  //int *major;
+//}haploCalls;
+
 class abcAMOVA:public abc{
-private:
-  int doAMOVA;
-  char *refName;
-  FILE *outfile;
-public:
-  void run(funkyPars  *pars);
-  void clean(funkyPars *pars);  
-  void print(funkyPars *pars);  
-  void openfile(const char *outfiles);
-  void getOptions(argStruct *arguments);
-  void printArg(FILE *argFile);
-  abcAMOVA(const char *outfiles,argStruct *arguments,int inputtype);
-  ~abcAMOVA();
+    private:
+        int doAMOVA;
+        char *refName;
+        FILE *outfile;
+        //non optional arguments
+        int doHaploCall;
+        int doCount;
+
+        //optional arguments
+        int maxMis;
+        int minMinor;
+
+        //out file
+        BGZF* outfileZ;
+
+        //functions
+        void printHaplo(funkyPars *pars);
+        void getHaplo(funkyPars *pars);
+
+        //print buffer
+        kstring_t bufstr;
+
+    public:
+        void run(funkyPars  *pars);
+        void clean(funkyPars *pars);  
+        void print(funkyPars *pars);  
+        void openfile(const char *outfiles);
+        void getOptions(argStruct *arguments);
+        void printArg(FILE *argFile);
+        abcAMOVA(const char *outfiles,argStruct *arguments,int inputtype);
+        ~abcAMOVA();
+
 };
 
 
