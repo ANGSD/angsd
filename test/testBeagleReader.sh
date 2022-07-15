50aa0a2f33f2bc3cc2c790f4551fd47b431eabef
isinaltinkaya <isinaltinkaya@gmail.com>
2022-07-15 14:36:14 +0200

    [fix,test] Fix printing log to logfile in testBeagleReader

--- test/testBeagleReader.sh
+++ test/testBeagleReader.sh
@@ -38,7 +38,7 @@ echo
 TDIR=beagle_reader
 REFDIR=${TDIR}/ref
 RESDIR=${TDIR}/test_results
-rm -rv ${RESDIR}
+rm -frv ${RESDIR}
 mkdir -pv ${RESDIR}
 
 
@@ -46,7 +46,7 @@ mkdir -pv ${RESDIR}
 
 
 
-${ANGSD} -doMaf 4 -beagle ${TDIR}/test1.beagle -fai ${REFDIR}/ref1.fa.fai -out ${RESDIR}/test1
+${ANGSD} -doMaf 4 -beagle ${TDIR}/test1.beagle -fai ${REFDIR}/ref1.fa.fai -out ${RESDIR}/test1 2>&1 >> ${LOG}
 RVAL=0;
 
 # 1_site
@@ -61,7 +61,7 @@ fi
 
 
 
-${ANGSD} -doMaf 4 -beagle ${TDIR}/test2.beagle -fai ${REFDIR}/ref2.fa.fai -out ${RESDIR}/test2
+${ANGSD} -doMaf 4 -beagle ${TDIR}/test2.beagle -fai ${REFDIR}/ref2.fa.fai -out ${RESDIR}/test2 2>&1 >> ${LOG}
 # chr1_site
 if [[ -n $($(cmp <(fz ${RESDIR}/test2.mafs.gz) <(f ${TDIR}/angsd_results/test2.mafs)) 2>> ${LOG}) ]];then
     echo "--------------"
@@ -72,7 +72,7 @@ if [[ -n $($(cmp <(fz ${RESDIR}/test2.mafs.gz) <(f ${TDIR}/angsd_results/test2.m
     RVAL=2
 fi
 
-${ANGSD} -doMaf 4 -beagle ${TDIR}/test3.beagle -fai ${REFDIR}/ref3.fa.fai -out ${RESDIR}/test3
+${ANGSD} -doMaf 4 -beagle ${TDIR}/test3.beagle -fai ${REFDIR}/ref3.fa.fai -out ${RESDIR}/test3 2>&1 >> ${LOG}
 # chr_1_site
 if [[ -n $($(cmp <(fzd ${RESDIR}/test3.mafs.gz 2) <(f ${TDIR}/angsd_results/test1.mafs)) 2>> ${LOG}) ]];then
     echo "--------------"
@@ -84,7 +84,7 @@ if [[ -n $($(cmp <(fzd ${RESDIR}/test3.mafs.gz 2) <(f ${TDIR}/angsd_results/test
 fi
 
 
-${ANGSD} -doMaf 4 -beagle ${TDIR}/test4.beagle -fai ${REFDIR}/ref4.fa.fai -out ${RESDIR}/test4
+${ANGSD} -doMaf 4 -beagle ${TDIR}/test4.beagle -fai ${REFDIR}/ref4.fa.fai -out ${RESDIR}/test4 2>&1 >> ${LOG}
 # chr_1_1_site
 if [[ -n $($(cmp <(fzd ${RESDIR}/test4.mafs.gz 3) <(f ${TDIR}/angsd_results/test1.mafs)) 2>> ${LOG}) ]];then
     echo "--------------"
@@ -96,7 +96,7 @@ if [[ -n $($(cmp <(fzd ${RESDIR}/test4.mafs.gz 3) <(f ${TDIR}/angsd_results/test
 fi
 
 
-${ANGSD} -doMaf 4 -beagle ${TDIR}/test5.beagle -fai ${REFDIR}/ref1.fa.fai -out ${RESDIR}/test5
+${ANGSD} -doMaf 4 -beagle ${TDIR}/test5.beagle -fai ${REFDIR}/ref1.fa.fai -out ${RESDIR}/test5 2>&1 >> ${LOG}
 # 1_site but \t separated
 if [[ -n $($(cmp <(fz ${RESDIR}/test5.mafs.gz) <(f ${TDIR}/angsd_results/test1.mafs)) 2>> ${LOG}) ]];then
     echo "--------------"
