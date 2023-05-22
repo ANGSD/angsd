8ecf807ff58d6cebc5a9fd67cc89540cc4d6b665
Isin Altinkaya <isinaltinkaya@gmail.com>
2023-05-22 20:23:20 +0200

    [fix] Add missing paranthesis to assert macro
    Add extra paranthesis to assert macro to avoid
    cases where ASSERT(x>y) expands to if(!x>y)
    instead of if(!(x>y))

--- aio.h
+++ aio.h
@@ -17,7 +17,7 @@
  * Macro:[ASSERT]
  * shortcut to evaluate an expression, works the same way as the C-macro assert
  */
-#define ASSERT(expr) if (!(expr)) {fprintf(stderr,"\n\n*******\n[ERROR](%s:%d) %s\n*******\n",__FILE__,__LINE__,#expr);exit(1);}
+#define ASSERT(expr) if (!((expr))) {fprintf(stderr,"\n\n*******\n[ERROR](%s:%d) %s\n*******\n",__FILE__,__LINE__,#expr);exit(1);}
 
 
 //angsd io
